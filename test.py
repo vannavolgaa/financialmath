ba066b458d47a4dfe47d4f4bc800d43f68668de9
vannavolgaa <marquesasarbitrage@gmail.com>
2023-12-12 00:09:12 +0100

    leaaaast square

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ bothfloat_lookback = LookBackPayoff(floating_strike=True, floating_spot=True,
                                  spot_method=LookbackMethod.geometric_mean, strike_method=LookbackMethod.arithmetic_mean, 
                                  spot_observation=ObservationType.continuous, strike_observation=ObservationType.continuous)
 opt_payoff = OptionPayoff(
-    option_type=OptionalityType.call,
+    option_type=OptionalityType.put,
     exercise=ExerciseType.european)
 opt_spec = OptionSpecification(100, OptionTenor(expiry=1))
 option = Option(opt_spec, opt_payoff)
@@ -30,7 +30,7 @@ q = 0.1
 t = 1 
 sigma = 0.2
 N = 100
-M = 50000
+M = 10000
 dt = t/N
 Bu = 120 
 
@@ -44,20 +44,50 @@ bsmc = MonteCarloBlackScholes(inputdata=mcinput)
 simulator = bsmc.get(False,False,False)
 sim = simulator.sim
 test = MonteCarloPricing(sim=sim, option=option, r=r)
-payoffs = test.compute_payoff()
+payoff_matrix = test.compute_payoff()
 
-pos = np.where(payoffs[:,N-2]>0)
-actual_payoff = payoffs[pos,N-2]
-Y = np.exp(-r*dt)*payoffs[pos[0],N-1]
-n_1_spot = sim[pos[0],N-2]
-n_1_spot_square =n_1_spot**2
-X = np.transpose(np.reshape(np.concatenate([n_1_spot, n_1_spot_square]),(2,len(pos[0]))))
 
-#xxt_inv = np.linalg.inv(np.dot(X, np.transpose(X)))
+def compute_continuation_payoff(discounted_payoff, spot, spot_squared, indexes):
+    n = len(indexes)
+    ones = np.ones(n)
+    Y = discounted_payoff[indexes]
+    output = np.zeros(len(discounted_payoff))
+    X_list = [ones, spot[indexes], spot_squared[indexes]]
+    X = np.transpose(np.reshape(np.concatenate(X_list),(len(X_list),n)))
+    coefs = np.linalg.lstsq(X, Y, rcond=None)[0]
+    cpayoff = np.transpose(X.dot(coefs))
+    output[indexes] = cpayoff
+    return output
 
-test3 = np.linalg.lstsq(X, Y, rcond=None)[0]
+payoff = payoff_matrix[:,N-1]
+dfdt = np.exp(-r*dt)
+for i in range(N-2,-1,-1): 
+    discounted_payoff = dfdt*payoff
+    actual_payoff, spot_vector = payoff_matrix[:,i], sim[:,i]
+    spot_squared_vector = spot_vector**2
+    indexes = np.where(actual_payoff>0)[0]
+    continuation_payoff = compute_continuation_payoff(
+        discounted_payoff,spot_vector,
+        spot_squared_vector,indexes)
+    exercise_indexes = actual_payoff>continuation_payoff
+    no_exercise_indexes = np.invert(exercise_indexes)
+    payoff[exercise_indexes] = actual_payoff[exercise_indexes]
+    payoff[no_exercise_indexes] = discounted_payoff[no_exercise_indexes]
 
-expected_payoff = np.transpose(X.dot(test3))
+
+price = dfdt*np.mean(payoff)
+
+
+
+
+
+
+
+
+
+
+
+
 
 @dataclass
 class MonteCarloLeastSquare: 
