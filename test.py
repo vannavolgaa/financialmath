0372d83ee2b7456d260bb33b1f25629e61042783
vannavolgaa <140949773+vannavolgaa@users.noreply.github.com>
2023-12-21 16:59:54 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,16 +1,42 @@
 import numpy as np 
 from financialmath.model.americanquadratic import (
-    QuadraticApproximationAmericanVanillaCall)
+    QuadraticApproximationAmericanVanilla)
 from financialmath.model.blackscholes.closedform import ClosedFormBlackScholesInput
+from financialmath.instruments.option import *
+from financialmath.pricing.option.pde import (
+    PDEBlackScholesValuation, 
+    PDEBlackScholesParameters
+)
 
-S = 100 
-r = -0.01
-q = 0.16
-t = 1
-sigma = np.array([0.2,0.5,0.49,0.48,0.21,0.27,0.39,0.255])
-K = 100 
+S = 110
+r = 0.12
+q = 0
+t = 0.25
+sigma = 0.2
+K = 100
+put = False 
+fut = True 
+if put: option_type = OptionalityType.put
+else: option_type = OptionalityType.call 
+opt_payoff = OptionPayoff(
+    option_type=option_type,
+    exercise=ExerciseType.american,
+    future=fut)
+opt_spec = OptionSpecification(K, OptionTenor(expiry=t))
+option = Option(opt_spec, opt_payoff)
 inputdata = ClosedFormBlackScholesInput(S=S, r=r, q=q, sigma=sigma, t=t, K=K)
-qame = QuadraticApproximationAmericanVanillaCall(inputdata=inputdata)
-test = qame.compute_prices()
+inputpde = PDEBlackScholesParameters(S=S, r=r, q=q, sigma=sigma, number_steps=200)
+qame = QuadraticApproximationAmericanVanilla(inputdata=inputdata, put=put, future=fut)
+pdepricer = PDEBlackScholesValuation(
+    option=option, 
+    parameters=inputpde, 
+    first_order_greek=False, second_order_greek=False, 
+    third_order_greek=False, max_workers=8)
+pdetest = pdepricer.valuation()
+print(pdetest.price)
+print(qame.compute_prices())
 
+qame.exercise_premium()
+#qame.initial_optimal_exercise_price()
 
+#qame.S_star
